9869311e3c7748561738add07145d687eaf94f0b
Kirill <kirezamba34@mail.ru>
2022-03-02 19:54:16 +0300

    refactored

--- src/components/layouts/main/MyLoader.jsx
+++ src/components/layouts/main/MyLoader.jsx
@@ -4,14 +4,14 @@ import ContentLoader from "react-content-loader";
 const MyLoader = (props) => (
   <ContentLoader
     speed={2}
-    width={400}
-    height={850}
-    viewBox="0 0 400 850"
+    width={500}
+    height={1000}
+    viewBox="0 0 500 1000"
     backgroundColor="#ade0f0"
     foregroundColor="#ecebeb"
     {...props}
   >
-    <rect x="0" y="0" rx="15" ry="15" width="400" height="850" />
+    <rect x="0" y="0" rx="15" ry="15" width="500" height="1000" />
   </ContentLoader>
 );
 
